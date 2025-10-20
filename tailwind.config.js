2315c441779a936c632a0166ede07cf5ecc4fc04
Samadul Amin <aminsamadul@gmail.com>
2025-10-20 14:27:52 +0600

    vite config changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+  './resources/**/*.{js,jsx,ts,tsx,vue}',
+  './resources/**/*.blade.php',
+  './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+  './storage/framework/views/*.php',
+]
+,
     theme: {
         extend: {
             fontFamily: {
