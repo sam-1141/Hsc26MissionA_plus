497fa313234be33940ee91a2318f51fa28878c82
enggr-sam <enggr.sam.ft@gmail.com>
2025-12-01 10:17:44 +0600

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
+    './resources/**/*.{js,js,ts,tsx,vue}', // all JS/React files
+    './resources/**/*.blade.php',           // all Blade templates
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php', // optional pagination
   ],
   theme: {
     extend: {
