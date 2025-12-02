340762b0156eaecc56844c124eb988707e17ccf8
enggr-sam <enggr.sam.ft@gmail.com>
2025-12-02 06:06:42 +0600

    simple 3

--- resources/js/Pages/Student/Exam/LiveExam/ExamMainPage.jsx
+++ resources/js/Pages/Student/Exam/LiveExam/ExamMainPage.jsx
@@ -1,15 +1,15 @@
 // ExamMainPageMinimal.jsx
-import ExamTimer from "./ExamTimer";
-import QuestionCard from "./QuestionCard";
-import FocusWarning from "../../../../components/FocusWarning";
+// import ExamTimer from "./ExamTimer";
+// import QuestionCard from "./QuestionCard";
+// import FocusWarning from "../../../components/FocusWarning";
 
 export default function ExamMainPageMinimal() {
   return (
     <div>
       <h1>Minimal Test</h1>
-      <ExamTimer duration={1} onTimeUp={() => {}} />
+      {/* <ExamTimer duration={1} onTimeUp={() => {}} />
       <QuestionCard question={{}} questionNumber={1} onAnswerSelect={() => {}} />
-      <FocusWarning maxWarnings={1} active={true} onMaxWarnings={() => {}} examId={1} />
+      <FocusWarning maxWarnings={1} active={true} onMaxWarnings={() => {}} examId={1} /> */}
     </div>
   );
 }
